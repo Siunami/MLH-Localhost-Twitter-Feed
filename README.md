1ea08bad0a5b26b23bc87324dfd13c1854165e32
Siunami <Siunami.matt@gmail.com>
2019-04-26 12:53:22 -0700

    basic project

---
+++ README.md
@@ -0,0 +1,21 @@
+# Introduction
+
+This is the starter code for the MLHLocalhost Twitter Feed
+
+# Installation
+
+This project requires the following tools:
+
+- Python - The programming language used by Flask.
+- Tweepy - A wrapper library for easily accessing the Twitter API
+
+To get started, install Python
+
+## Getting Started
+
+**Step 1. Clone this repo into a fresh folder**
+
+```
+$ git clone https://github.com/Siunami/MLH-Localhost-Twitter-Feed.git
+$ cd MLH-Localhost-Twitter-Feed
+```
---
+++ tweeter.py
@@ -0,0 +1,33 @@
+import tweepy
+import re
+from keys import apiKey,apiKeySecret,accessToken,accessTokenSecret
+
+class Tweeter:
+    def __init__(self):
+        self.api = self.authenticate()
+        self.tweets = []
+
+    def authenticate(self):
+        auth = tweepy.OAuthHandler(apiKey, apiKeySecret)
+        auth.set_access_token(accessToken, accessTokenSecret)
+        return tweepy.API(auth)
+
+    
+    def scrapeTweets(self):
+        tweets = []
+        pattern = re.compile("#MLHLocalhost")
+
+        # https://tweepy.readthedocs.io/en/latest/cursor_tutorial.html
+        # Cursor instead of api call
+        # Neex to use tweet_mode and full_text instead of text
+        for tweet in tweepy.Cursor(self.api.user_timeline,id='mlhacks',tweet_mode='extended').items(100):
+            if pattern.search(tweet.full_text):
+                tweets.append({
+                    "text":tweet.full_text,
+                    "date":tweet.created_at
+                })
+        self.tweets = tweets
+    
+    def getTweets(self):
+        return self.tweets
+    
