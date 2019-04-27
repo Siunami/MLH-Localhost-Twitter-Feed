dc66b3e8e7db7621aaa83ff19d8e5a670b4ca464
Siunami <Siunami.matt@gmail.com>
2019-04-27 11:28:51 -0700

    final project code

---
+++ final-project/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from tweeter import Tweeter
+import os
+
+app = Flask(__name__)
+
+tweetObject = Tweeter("#MLHLocalhost",20)
+tweetObject.scrapeTweets()
+
+@app.route('/')
+def homepage():
+    return render_template('index.html', tweets=tweetObject.getTweets())
---
+++ final-project/tweeter.py
@@ -0,0 +1,29 @@
+import tweepy
+from keys import apiKey,apiKeySecret,accessToken,accessTokenSecret
+
+class Tweeter:
+    def __init__(self, search_query, numTweets):
+        self.search_query = search_query
+        self.numTweets = numTweets
+        self.api = self.authenticate()
+        self.tweets = []
+
+    def authenticate(self):
+        auth = tweepy.OAuthHandler(apiKey, apiKeySecret)
+        auth.set_access_token(accessToken, accessTokenSecret)
+        return tweepy.API(auth)
+    
+    def scrapeTweets(self):
+        cursor = tweepy.Cursor(self.api.search, q=self.search_query,tweet_mode='extended')
+        for tweet in cursor.items(self.numTweets):
+            data = {
+                "text":tweet.full_text,
+                "date":tweet.created_at,
+                "user":tweet.user.name,
+                "url":"https://twitter.com/" + tweet.user.screen_name
+            }
+            self.tweets.append(data)
+
+    def getTweets(self):
+        return self.tweets
+    
