6b10eba952eb6acecc5fd415921145adb1183f2e
Siunami <Siunami.matt@gmail.com>
2019-04-27 01:48:58 -0700

    bind to port

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ tweetObject.scrapeTweets()
 @app.route('/')
 def homepage():
     return render_template('index.html', tweets=tweetObject.getTweets())
+
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, port)
