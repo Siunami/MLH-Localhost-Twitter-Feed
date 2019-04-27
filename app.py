aa9465b4fc73a6485d812297ec61e92f742343c5
Siunami <Siunami.matt@gmail.com>
2019-04-27 01:50:13 -0700

    bind to port

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def homepage():
     return render_template('index.html', tweets=tweetObject.getTweets())
 
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, port)
+app.run(debug=True, port=port)
